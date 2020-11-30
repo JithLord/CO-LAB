376847c396497a114ecb7d2e0d112b528e4d76ca
Jithin Nambiar <45201620+JithLord@users.noreply.github.com>
2020-11-30 17:15:48 +0530

    Update bools.py

--- week-9/bools.py
+++ week-9/bools.py
@@ -5,27 +5,26 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(33, GPIO.OUT)  #Output
 GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #Input 1
 GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #Input 2
-GPIO.output(11, GPIO.HIGH)
 
 try:
     while(1):
         a,b = GPIO.input(35),GPIO.input(37)
         c = a or b
         if c:
-            GPIO.output(11, GPIO.HIGH)
+            GPIO.output(33, GPIO.HIGH)
         else:
-            GPIO.output(11, GPIO.LOW)        
+            GPIO.output(33, GPIO.LOW)        
 except KeyboardInterrupt:
-    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(33, GPIO.HIGH)
     
 try:
     while(1):
         a,b = GPIO.input(35),GPIO.input(37)
         c = a or b
         if c:
-            GPIO.output(11, GPIO.HIGH)
+            GPIO.output(33, GPIO.HIGH)
         else:
-            GPIO.output(11, GPIO.LOW)
+            GPIO.output(33, GPIO.LOW)
         
 except KeyboardInterrupt:
-    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(33, GPIO.HIGH)
